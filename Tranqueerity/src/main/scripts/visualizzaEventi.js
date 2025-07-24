a3fb7e5ea108d22954e96bd4b0ed8bf8c4d2eb5a
miria <m.desantis40@studenti.unisa.it>
2025-07-24 19:03:12 +0200

    Correzzione nella visualizzazione degli eventi. Definizione completa, da un punto di vista grafico

--- Tranqueerity/src/main/scripts/visualizzaEventi.js
+++ Tranqueerity/src/main/scripts/visualizzaEventi.js
@@ -1,35 +1,49 @@
 function creaEvento(event){
     //recupero codide della card;
     const card = document.createElement('div');
+    card.className = 'card';
+
 
     const cardInfo = document.createElement('div');
     cardInfo.className = 'card-info';
+
+    // Contenitore 2x2 per titolo e orario;
+    const row1 = document.createElement('div');
+    row1.className = 'card-row';
 
     //titolo dell'evento
     const title = document.createElement('h2');
     title.className = 'eventTitle';
     title.textContent = event.name;
 
+    const time=document.createElement('p');
+    time.className="OrarioEvento";
+    time.textContent = "🕒 " + event.date; //orario dell'evento
+
+    row1.appendChild(title);
+    row1.appendChild(time);
+
+    //contenitore 2x2 per descrizione e luogo;
+    const row2 = document.createElement('div');
+    row2.className = 'card-row';
+
     //descrizione dell'evento
     const description = document.createElement('p');
     description.className = 'cardDescription';
     description.textContent = event.description;
 
-    const time=document.createElement('p');
-    time.className="OrarioEvento";
-    time.textContent = event.date; //orario dell'evento
-
     const place=document.createElement('p');
     place.className="LuogoEvento";
-    place.textContent= event.luogo; //luogo dell'evento
+    place.textContent = "📍 " + event.luogo; //luogo dell'evento
 
     const icons = document.createElement('div');
-    icons.className = 'card-icons';
 
-    cardInfo.appendChild(title);
-    cardInfo.appendChild(description);
-    cardInfo.appendChild(time);
-    cardInfo.appendChild(place);
+    row2.appendChild(description);
+    row2.appendChild(place);
+
+    // Assemblaggio finale
+    cardInfo.appendChild(row1);
+    cardInfo.appendChild(row2);
 
     card.appendChild(cardInfo); //attacca il nuovo elemento a quelli già esistenti;
     card.appendChild(icons);
